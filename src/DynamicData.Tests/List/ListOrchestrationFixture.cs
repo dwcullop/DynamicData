dd0fe2795ca0277d5efc5fcf6aabd719fc1c0e62
Darrin W. Cullop <Darrin.Cullop@microsoft.com>
2026-06-14 17:20:34 -0700

    Add ListOrchestration tests and RecordingListOrchestrator helper
    12 unit tests covering source routing, Track/Untrack, completion
    accounting, error propagation, OnDrainComplete, Serialize, slot
    replacement via Track, disposal, and construction error rollback.

---
+++ src/DynamicData.Tests/List/ListOrchestrationFixture.cs
@@ -0,0 +1,372 @@
+﻿// Copyright (c) 2011-2025 Roland Pheasant. All rights reserved.
+// Roland Pheasant licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+
+using DynamicData.Kernel;
+using DynamicData.List.Internal;
+using DynamicData.Tests.Utilities;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace DynamicData.Tests.List;
+
+public sealed class ListOrchestrationFixture
+{
+    [Fact]
+    public void SourceChangeSets_AreRoutedToOrchestrator()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        RecordingListOrchestrator<string, int, int>? orch = null;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+            {
+                orch = new RecordingListOrchestrator<string, int, int>();
+                return orch;
+            })
+            .Subscribe();
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "b", 1) });
+
+        orch.Should().NotBeNull();
+        orch!.SourceChangeSets.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public void Track_RoutesInnerEmissionsThroughOnInner()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        using var inner = new Subject<int>();
+        RecordingListOrchestrator<string, int, int>? orch = null;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+            {
+                orch = new RecordingListOrchestrator<string, int, int>
+                {
+                    OnSourceChangeSetHook = (changes, c) =>
+                    {
+                        foreach (var change in changes)
+                        {
+                            if (change.Reason == ListChangeReason.Add)
+                            {
+                                c.Track(change.Item.Current, inner);
+                            }
+                        }
+                    }
+                };
+                return orch;
+            })
+            .Subscribe();
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+        inner.OnNext(42);
+        inner.OnNext(100);
+
+        orch.Should().NotBeNull();
+        orch!.Inners.Should().HaveCount(2);
+        orch.Inners[0].Value.Should().Be(42);
+        orch.Inners[0].Slot.Item.Should().Be("a");
+        orch.Inners[1].Value.Should().Be(100);
+    }
+
+    [Fact]
+    public void Untrack_DisposesInnerSubscription()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        using var inner = new Subject<int>();
+        RecordingListOrchestrator<string, int, int>? orch = null;
+        IListSlot<string>? trackedSlot = null;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+            {
+                orch = new RecordingListOrchestrator<string, int, int>
+                {
+                    OnSourceChangeSetHook = (changes, c) =>
+                    {
+                        foreach (var change in changes)
+                        {
+                            if (change.Reason == ListChangeReason.Add)
+                            {
+                                trackedSlot = change.Item.Current;
+                                c.Track(trackedSlot, inner);
+                            }
+                            else if (change.Reason == ListChangeReason.Remove)
+                            {
+                                c.Untrack(change.Item.Current);
+                            }
+                        }
+                    }
+                };
+                return orch;
+            })
+            .Subscribe();
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+        inner.OnNext(1);
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Remove, "a", 0) });
+        inner.OnNext(2);
+        inner.OnNext(3);
+
+        orch.Should().NotBeNull();
+        orch!.Inners.Should().HaveCount(1, "only the pre-Untrack emission should reach OnInner");
+        orch.Inners[0].Value.Should().Be(1);
+    }
+
+    [Fact]
+    public void SourceCompleted_WithNoInners_FiresDownstreamOnCompleted()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        var completed = false;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+                new RecordingListOrchestrator<string, int, int>())
+            .Subscribe(_ => { }, _ => { }, () => completed = true);
+
+        source.OnCompleted();
+
+        completed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void SourceCompleted_WithLiveInners_DefersOnCompletedUntilInnersComplete()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        using var inner = new Subject<int>();
+        var completed = false;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+                new RecordingListOrchestrator<string, int, int>
+                {
+                    OnSourceChangeSetHook = (changes, c) =>
+                    {
+                        foreach (var change in changes)
+                        {
+                            if (change.Reason == ListChangeReason.Add)
+                            {
+                                c.Track(change.Item.Current, inner);
+                            }
+                        }
+                    }
+                })
+            .Subscribe(_ => { }, _ => { }, () => completed = true);
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+        source.OnCompleted();
+
+        completed.Should().BeFalse("inner still alive");
+
+        inner.OnCompleted();
+
+        completed.Should().BeTrue("inner completion brought subscription count to zero");
+    }
+
+    [Fact]
+    public void SourceError_PropagatesToDownstream()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        Exception? receivedError = null;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+                new RecordingListOrchestrator<string, int, int>())
+            .Subscribe(_ => { }, ex => receivedError = ex);
+
+        var boom = new Exception("source failed");
+        source.OnError(boom);
+
+        receivedError.Should().BeSameAs(boom);
+    }
+
+    [Fact]
+    public void OrchestratorThrowsInOnSourceChangeSet_PropagatesAsOnError()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        Exception? receivedError = null;
+        var boom = new InvalidOperationException("orchestrator failed");
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+                new RecordingListOrchestrator<string, int, int>
+                {
+                    OnSourceChangeSetHook = (_, _) => throw boom
+                })
+            .Subscribe(_ => { }, ex => receivedError = ex);
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+
+        receivedError.Should().BeSameAs(boom);
+    }
+
+    [Fact]
+    public void OnDrainComplete_FiresWithIsFinalTrueAfterFullTermination()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        RecordingListOrchestrator<string, int, int>? orch = null;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+            {
+                orch = new RecordingListOrchestrator<string, int, int>();
+                return orch;
+            })
+            .Subscribe();
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+        source.OnCompleted();
+
+        orch.Should().NotBeNull();
+        orch!.DrainCompletes.Should().NotBeEmpty();
+        orch.DrainCompletes.Last().IsFinal.Should().BeTrue("source completed with no live inners");
+    }
+
+    [Fact]
+    public void Serialize_RoutesObservableThroughGateWithoutCompletionAccounting()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        using var auxiliary = new Subject<int>();
+        var auxValues = new List<int>();
+        var completed = false;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+                new RecordingListOrchestrator<string, int, int>
+                {
+                    OnSourceChangeSetHook = (changes, c) =>
+                    {
+                        foreach (var change in changes)
+                        {
+                            if (change.Reason == ListChangeReason.Add)
+                            {
+                                c.Serialize(auxiliary).Subscribe(v => auxValues.Add(v));
+                            }
+                        }
+                    }
+                })
+            .Subscribe(_ => { }, _ => { }, () => completed = true);
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+        auxiliary.OnNext(1);
+        auxiliary.OnNext(2);
+
+        source.OnCompleted();
+
+        completed.Should().BeTrue("Serialize does not participate in completion accounting; source completion alone completes downstream");
+        auxValues.Should().BeEquivalentTo(new[] { 1, 2 });
+    }
+
+    [Fact]
+    public void Track_OnSameSlotTwice_ReplacesInnerSubscription()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        using var first = new Subject<int>();
+        using var second = new Subject<int>();
+        RecordingListOrchestrator<string, int, int>? orch = null;
+        IListSlot<string>? slot = null;
+        var calls = 0;
+
+        using var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+            {
+                orch = new RecordingListOrchestrator<string, int, int>
+                {
+                    OnSourceChangeSetHook = (changes, c) =>
+                    {
+                        foreach (var change in changes)
+                        {
+                            if (change.Reason == ListChangeReason.Add)
+                            {
+                                slot = change.Item.Current;
+                                calls++;
+                                c.Track(slot, calls == 1 ? first : second);
+                            }
+                            else if (change.Reason == ListChangeReason.Refresh)
+                            {
+                                calls++;
+                                c.Track(slot!, second);
+                            }
+                        }
+                    }
+                };
+                return orch;
+            })
+            .Subscribe();
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+        first.OnNext(10);
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Refresh, "a", Optional<string>.None, 0) });
+        first.OnNext(20);
+        second.OnNext(30);
+
+        orch.Should().NotBeNull();
+        orch!.Inners.Select(i => i.Value).Should().BeEquivalentTo(new[] { 10, 30 }, opts => opts.WithStrictOrdering());
+    }
+
+    [Fact]
+    public void Disposal_StopsAllCallbacks()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        using var inner = new Subject<int>();
+        RecordingListOrchestrator<string, int, int>? orch = null;
+
+        var sub = source
+            .Orchestrate<string, int, int>((ctx, observer) =>
+            {
+                orch = new RecordingListOrchestrator<string, int, int>
+                {
+                    OnSourceChangeSetHook = (changes, c) =>
+                    {
+                        foreach (var change in changes)
+                        {
+                            if (change.Reason == ListChangeReason.Add)
+                            {
+                                c.Track(change.Item.Current, inner);
+                            }
+                        }
+                    }
+                };
+                return orch;
+            })
+            .Subscribe();
+
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "a", 0) });
+        inner.OnNext(1);
+
+        sub.Dispose();
+
+        inner.OnNext(2);
+        source.OnNext(new ChangeSet<string> { new(ListChangeReason.Add, "b", 1) });
+
+        orch.Should().NotBeNull();
+        orch!.Inners.Should().HaveCount(1, "no callbacks should fire after disposal");
+        orch.SourceChangeSets.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void ConstructionError_RollsBackResources()
+    {
+        using var source = new Subject<IChangeSet<string>>();
+        var boom = new InvalidOperationException("factory failed");
+
+        Action act = () => source
+            .Orchestrate<string, int, int>((ctx, observer) => throw boom)
+            .Subscribe();
+
+        act.Should().Throw<InvalidOperationException>().WithMessage("factory failed");
+
+        // If we got here without leaking resources, the test passes. There's no externally
+        // observable handle to the leaked queue, so this test mainly proves the throw escapes
+        // cleanly without crashing the runtime.
+    }
+}
---
+++ src/DynamicData.Tests/Utilities/RecordingListOrchestrator.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2011-2025 Roland Pheasant. All rights reserved.
+// Roland Pheasant licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+
+using DynamicData.List.Internal;
+
+namespace DynamicData.Tests.Utilities;
+
+/// <summary>
+/// Recording orchestrator used for unit-testing <see cref="ListOrchestration{TSource,TInner,TResult}"/>
+/// in isolation. Captures every callback the driver invokes and exposes hooks for tests to drive
+/// downstream emissions or trigger errors.
+/// </summary>
+internal sealed class RecordingListOrchestrator<TSource, TInner, TResult>
+    : IListOrchestrator<TSource, TInner, TResult>
+    where TSource : notnull
+{
+    public List<IChangeSet<IListSlot<TSource>>> SourceChangeSets { get; } = new();
+
+    public List<InnerEmission> Inners { get; } = new();
+
+    public List<DrainCompleteCall> DrainCompletes { get; } = new();
+
+    public Action<IChangeSet<IListSlot<TSource>>, IListOrchestratorContext<TSource, TInner>>? OnSourceChangeSetHook { get; set; }
+
+    public Action<TInner, IListSlot<TSource>, IObserver<TResult>>? OnInnerHook { get; set; }
+
+    public Action<bool, IObserver<TResult>>? OnDrainCompleteHook { get; set; }
+
+    public void OnSourceChangeSet(IChangeSet<IListSlot<TSource>> changes, IListOrchestratorContext<TSource, TInner> context)
+    {
+        SourceChangeSets.Add(changes);
+        OnSourceChangeSetHook?.Invoke(changes, context);
+    }
+
+    public void OnInner(TInner value, IListSlot<TSource> slot, IObserver<TResult> emitter)
+    {
+        Inners.Add(new InnerEmission(value, slot));
+        OnInnerHook?.Invoke(value, slot, emitter);
+    }
+
+    public void OnDrainComplete(bool isFinal, IObserver<TResult> emitter)
+    {
+        DrainCompletes.Add(new DrainCompleteCall(isFinal));
+        OnDrainCompleteHook?.Invoke(isFinal, emitter);
+    }
+
+    public sealed record InnerEmission(TInner Value, IListSlot<TSource> Slot);
+
+    public sealed record DrainCompleteCall(bool IsFinal);
+}
